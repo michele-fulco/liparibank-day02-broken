db955d802a56712f31d1b3a827a56db4d63be7dc
Michele <michele.fulco@liparipeople.com>
2026-04-05 17:32:32 +0200

    feat(security);: aggiunto file di configurazione di SpringSecurity

---
+++ src/main/java/com/lipari/bank/security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.lipari.bank.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
